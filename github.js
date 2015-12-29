ee8eb5b4b06c46a75fd29f04e50deb02ea7ddb85
Nir Peled <nir@whipclip.com>
2015-12-29 16:28:52 +0200

    [FEAT] add a form username template with handlers

--- github.js
+++ github.js
@@ -16,4 +16,23 @@ CLIENT
 -----------------------------------------------------
 */
 
-if (Meteor.isClient) {}
+if (Meteor.isClient) {
+
+    console.log('[GitHub] init');
+
+    Template.formUsername.events({
+
+        // handle username input
+        'change input': function (e, template) {
+            Session.set('username', e.target.value);
+            console.log('[GitHub] username has changed to ' + Session.get('username'));
+        },
+
+        // handle fetch button
+        'click button': function(e, template) {
+            console.log('[GitHub] fetching repos for username ' + Session.get('username'));
+        }
+
+    });
+
+}
