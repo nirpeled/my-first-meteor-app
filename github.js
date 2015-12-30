902c201323a89a6bc885a40209535203aa21994c
Nir Peled <nir@whipclip.com>
2015-12-30 02:22:38 +0200

    [FIX] improve the UI/UX of the main form

--- github.js
+++ github.js
@@ -38,43 +38,62 @@ CLIENT
 
 if (Meteor.isClient) {
 
-    console.log('[GitHub] init');
+    // formUsername
 
-    Template.repos.helpers({
-        repos: function() {
-            return Session.get('repos')
+    Template.formUsername.helpers({
+
+        isLoading: function() {
+          return Session.get('isLoading');
         }
+
     });
 
     Template.formUsername.events({
 
-        // handle username input
         'change input': function (e, template) {
             Session.set('username', e.target.value);
-            console.log('[GitHub] username has changed to ' + Session.get('username'));
         },
 
-        // handle fetch button
-        'click button': function(e, template) {
+        'click i': function(e, template) {
+            template.$('form').submit();
+        },
+
+        'submit form': function(e, template) {
+
+            e.preventDefault();
 
             var username = Session.get('username');
 
-            console.log('[GitHub] fetching repos for username ' + username);
+            if (!username) {
+                return;
+            }
 
-            // TODO: validate username
+            console.log('[formUsername] fetching repos for username ' + username);
 
-            // TODO: use meteor template helpers
+            Session.set('isLoading', true);
 
             Meteor.call('getRepos', username, function(error, response){
 
-                console.log('[GitHub] fetching repos for username ' + username + ' : done');
+                console.log('[formUsername] fetching repos for username ' + username + ' : done');
 
                 console.log(response);
+
+                Session.set('isLoading', false);
 
                 Session.set('repos', response);
 
             });
 
+        }
+
+    });
+
+    // repos
+
+    Template.repos.helpers({
+
+        repos: function() {
+            return Session.get('repos');
         }
 
     });
