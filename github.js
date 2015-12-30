68fd33a6345713ef2960a7808297e84a4b2ac59b
Nir Peled <nir@whipclip.com>
2015-12-30 15:20:03 +0200

    [FIX] move repos data logic to client-side + improve UI

--- github.js
+++ github.js
@@ -14,17 +14,13 @@ if (Meteor.isServer) {
 
         'getRepos':function(username) {
 
-            var repos = Async.runSync(function(done) {
+            var response = Async.runSync(function(done) {
                 github.repos.getFromUser({user: username}, function(error, data) {
                     done(null, data);
                 });
             });
 
-            lodash.forEach(repos.result, function(repo, index) {
-                repos.result[index].score = repo.forks_count + (2 * repo.stargazers_count) + repo.watchers_count;
-            });
-
-            return repos.result;
+            return response;
 
         }
 
@@ -52,9 +48,9 @@ if (Meteor.isClient) {
 
     Template.formUsername.events({
 
-        'change input': function (e, template) {
-            Session.set('username', e.target.value);
-        },
+        //'change input': function (e, template) {
+        //    Session.set('username', e.target.value);
+        //},
 
         'click i': function(e, template) {
             template.$('form').submit();
@@ -64,17 +60,30 @@ if (Meteor.isClient) {
 
             e.preventDefault();
 
-            var username = Session.get('username');
+            var username = template.$('input').val();
 
             if (!username) {
                 return;
             }
 
-            Session.set('isLoading', true);
+            Session.set({
+                isLoading: true,
+                username: username
+            });
 
             Meteor.call('getRepos', username, function(error, response){
 
-                response = lodash.sortByOrder(response, 'score', 'desc');
+                if (lodash.size(response.result)) {
+
+                    // calculate a repository score
+                    lodash.forEach(response.result, function(repo, index) {
+                        response.result[index].score = repo.forks_count + (2 * repo.stargazers_count) + repo.watchers_count;
+                    });
+
+                    // sort repositories by score (descending)
+                    response.result = lodash.sortByOrder(response.result, 'score', 'desc');
+
+                }
 
                 Session.set({
                     isLoading: false,
@@ -93,6 +102,10 @@ if (Meteor.isClient) {
 
         repos: function() {
             return Session.get('repos');
+        },
+
+        username: function() {
+            return Session.get('username');
         },
 
         moment: function(date) {
